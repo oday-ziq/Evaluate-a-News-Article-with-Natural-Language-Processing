3c6798a8ef283d9aa8d74924a701f3b0fa746533
oday ziq <166035121+oday-ziq@users.noreply.github.com>
2025-02-14 16:37:00 +0200

    Update nameChecker.spec.js

--- test/nameChecker.spec.js
+++ test/nameChecker.spec.js
@@ -1,11 +1,17 @@
 import { isValidURL } from "../src/client/js/nameChecker";
 
-describe("Testing the URL validation function", () => {
-    test("Valid URL should return true", () => {
-        expect(isValidURL("https://www.google.com")).toBe(true);
+describe("Testing URL validation", () => {
+    test("isValidURL function should be defined", () => {
+        expect(isValidURL).toBeDefined();
     });
 
-    test("Invalid URL should return false", () => {
+    test("Valid URLs should return true", () => {
+        expect(isValidURL("https://www.google.com")).toBe(true);
+        expect(isValidURL("http://example.org")).toBe(true);
+    });
+
+    test("Invalid URLs should return false", () => {
         expect(isValidURL("invalid-url")).toBe(false);
+        expect(isValidURL("www.example")).toBe(false);
     });
 });
